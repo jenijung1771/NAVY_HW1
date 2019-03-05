3e90a302ffd2f08f4f5e5b13e664f73191fd953b
Robert <rriselvato@cs.stonybrook.edu>
2019-03-04 22:29:04 -0500

    GameLog and LogPage now display games details
    Signed-off-by: RobRis75830 <rriselvato@cs.stonybrook.edu>

--- team_navy_hw1/src/main/java/navy_hw1/demo/PostsController.java
+++ team_navy_hw1/src/main/java/navy_hw1/demo/PostsController.java
@@ -1,12 +1,13 @@
 package navy_hw1.demo;
 
-import entities.GameRepository;
+import entities.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import entities.PlayerRepository;
+
+import java.util.List;
 
 @Controller
 @EnableAutoConfiguration
@@ -16,6 +17,9 @@ public class PostsController {
 
     @Autowired
     private GameRepository gameRepository;
+
+    @Autowired
+    private PlaysRepository playsRepository;
 
     @RequestMapping(value = "/posts", method = RequestMethod.GET)
     public String newPost(Model model) {
@@ -60,6 +64,13 @@ public class PostsController {
     @RequestMapping(value = "/GameLog", method = RequestMethod.POST)
     public String postLog(@ModelAttribute("post") Post post) {
         return "GameLog";
+    }
+
+    @RequestMapping(value = "/ViewDetails", method = RequestMethod.GET)
+    public String getDetails(@RequestParam(name="gameid") int gameID, Model model) {
+        List<Plays> turns = playsRepository.findByPlaysIDGameID(gameID);
+        model.addAttribute("details", turns);
+        return "LogPage";
     }
 
 }
